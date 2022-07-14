48624cc43d3113da12201de1ce01273fa42531e1
shubham naik <103191352+shubahm24jhdp@users.noreply.github.com>
2022-07-14 19:53:02 +0530

    Create pr_12_2.cpp

---
+++ pr_12_2.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+clrscr();
+cout<<"    *    "<<endl;
+cout<<"   ***   "<<endl;
+cout<<"  *****  "<<endl;
+cout<<" ******* "<<endl;
+cout<<"*********"<<endl;
+getch();
+return 0;
+}
