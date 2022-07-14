ae2865ca27fba903025b0eb4dbe3a586130fb941
shubham naik <103191352+shubahm24jhdp@users.noreply.github.com>
2022-07-14 19:47:50 +0530

    Create pr_5_2.cpp

---
+++ pr_5_2.cpp
@@ -0,0 +1,17 @@
+#include<iostram.h>
+#include<conio.h>
+#include<iomainip.h>
+int a=5;
+class student;
+public;
+
+student()
+{
+c++;
+}
+int r_no;
+char name;
+int age;
+char br;
+static int c;
+void
