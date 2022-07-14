15e44af717f24565dfe124bfc6aabf3fb887cef0
shubham naik <103191352+shubahm24jhdp@users.noreply.github.com>
2022-07-14 19:16:05 +0530

    Create pr_2-1.cpp

---
+++ pr_2-1.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int a,b,c,d;
+clrscr();
+cout<<"enter the value of a:";
+cin>>a;
+cout<<"enter the value of b:";
+cin>>b;
+c=a*b;
+d=a/b;
+cout<<"ans."<<c;
+cout<<"ans."<<d;
+getch();
+return 0;
+}
