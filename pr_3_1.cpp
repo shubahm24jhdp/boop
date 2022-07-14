9916b63069185d1b42a75cd59e3005e5d46fe280
shubham naik <103191352+shubahm24jhdp@users.noreply.github.com>
2022-07-14 20:03:58 +0530

    Create pr_3_1.cpp

---
+++ pr_3_1.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+int main() 
+{
+float temp=15.65789;
+Clrscr() ;
+Cout<<setfill() ;
+Cout<<setw(20) ;
+Cout<<hello world "<<endl;
+cout<<setprecision(2) ;
+Cout<<temp;
+Return 0;
+}
