f0b922dd590529bacbb5f253dec6e71eeff9165f
shubham naik <103191352+shubahm24jhdp@users.noreply.github.com>
2022-07-14 19:39:03 +0530

    Create pr_11_2.cpp

---
+++ pr_11_2.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+const int a=5;
+const float b=3.14;
+const char c='z';
+
+clrscr();
+cout<<"integar const:"<<a<<endl;
+cout<<"float const:"<<b<<endl;
+cout<<"character const:"<<c<<endl;
+
+
+
+return 0;
+}
