463feebda781ae674d7c9f6ba8f7a20628e4b6e8
shubham naik <103191352+shubahm24jhdp@users.noreply.github.com>
2022-07-14 19:32:30 +0530

    Create pr_10_2.cpp

---
+++ pr_10_2.cpp
@@ -0,0 +1,22 @@
+#include<iostream.h>
+#include<conio.h>
+int C=0;
+class ob
+{
+public:
+ob()
+{
+C++;
+cout<<"object of class:"<<C<<endl;
+}
+};
+int main()
+{
+clrscr();
+ob d;
+ob h;
+ob r;
+ob u;
+ob v;
+return 0;
+}
